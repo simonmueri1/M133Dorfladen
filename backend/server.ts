100567ce0c09b119b729249f6445306368f2ae42
simonmueri1 <simon.mueri@schindler.com>
2020-01-17 21:12:53 +0100

    create chekout

--- backend/server.ts
+++ backend/server.ts
@@ -33,6 +33,10 @@ app.get("/shoppingcart", (req, res) => {
     res.sendFile(path.join(__dirname + '/../frontend/html/shoppingcart.html'));
 });
 
+app.get("/checkout", (req, res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/html/checkout.html'));
+})
+
 
 //api
 app.get("/api/products", (req, res) => {
